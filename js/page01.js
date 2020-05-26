e69ce9a38c63bb8a08ace459d7a58c95c992bebb
Patrick William Cavalheiro <pwcavalheiro@gmail.com>
2020-05-26 19:49:14 -0300

    Form Cadastro Usuarios Adicionado, Form Validation implementado - movido o page01.js da raiz para a pasta js - adicionado plugin jquery.validate.min.js

---
+++ js/page01.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $('#btn-abrir').click(function(){
+        $('#meu-modal').modal('show');
+    });
+
+
+    $('#btn-fechar').click(function(){
+        $('#meu-modal').modal('hide');
+    });
+
+    $('[data-toggle=tooltip]').tooltip();
+    $('[data-toggle=popover]').popover({html: true, placement: 'top'});
+
+    $('.btn-excluir').click(function(){
+        //remove a linha...
+        $(this).parents('tr').remove();
+
+        //mostra a mensagem
+        $('#message-cliente').toast({
+            autohide: true,
+            delay: 5000
+        }).toast('show');
+    });
+
+    $('#form-user').validate({
+        rules: {
+            nome: "required",
+            senha: "required",
+            field_email: {
+                required: true,
+                email: true
+            },
+            idade: {
+                number: true,
+                min: 16,
+                max: 100
+            }
+        },
+        messages: {
+            nome: "Por favor, preencha seu nome...",
+            senha: "Por favor, preencha sua senha...",
+            field_email: {
+                required: "Por favor, preencha seu e-mail...",
+                email: "Insira um e-mail válido!"
+            }, 
+            idade: {
+                number: "Insira apenas números...",
+                min: "Idade mínima permitida: 16 anos",                
+                max: "Idade máxima permitida: 100 anos"
+            }
+        },  
+        errorPlacement: function(error, element) {            
+            error.insertAfter(element).addClass('text-danger');
+        },
+        errorClass: "is-invalid" 
+    });
+});
